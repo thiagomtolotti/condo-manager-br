add1129f68b78189342801a9baa3698963edcd8e
Thiago Tolotti <thiagotolotti@gmail.com>
2025-05-17 13:30:02 -0300

    loads environment on server start

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	routes "backend/router"
+	"backend/utils"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
@@ -12,8 +13,12 @@ const PORT = "8080"
 func main() {
 	router := gin.Default()
 
+	utils.LoadEnvironment()
 	routes.RegisterRoutes(router)
 
+	fmt.Println()
 	fmt.Println("Starting server on port " + PORT)
+	fmt.Println()
+	fmt.Println()
 	router.Run(":" + PORT)
 }
---
+++ backend/utils/LoadEnvironment.go
@@ -0,0 +1,16 @@
+package utils
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvironment() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file: ", err)
+	}
+
+}
