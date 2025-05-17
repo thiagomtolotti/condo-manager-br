48a0d749945451a79972b23f5762d4e6dc9d1eae
Thiago Tolotti <thiagotolotti@gmail.com>
2025-05-17 19:27:36 -0300

    validates delete id before attempting to delete resource

--- backend/controllers/apartamento/Delete.go
+++ backend/controllers/apartamento/Delete.go
@@ -6,14 +6,28 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func Delete(c *gin.Context) {
 	id := c.Param("id")
 
-	if len(strings.TrimSpace(id)) == 0 {
+	if !validateId(id) {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "É necessário fornecer um id válido"})
+		return
 	}
 
 	fmt.Println("Deleting apartment with id ", id)
+}
+
+func validateId(id string) bool {
+	if len(strings.TrimSpace(id)) == 0 {
+		return false
+	}
+
+	if err := uuid.Validate(id); err != nil {
+		return false
+	}
+
+	return true
 }
