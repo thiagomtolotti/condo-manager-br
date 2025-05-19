df46399bd9784abd453900ad2fc86f6493ded07e
Thiago Tolotti <thiagotolotti@gmail.com>
2025-05-18 23:27:04 -0300

    fixes binding required in bool fields (validation error when false)

--- backend/schemas/morador.go
+++ backend/schemas/morador.go
@@ -6,8 +6,8 @@ type MoradorWithoutCPF struct {
 	Apartamento_id uuid.UUID `json:"apartamento_id" binding:"required"`
 	Nome           string    `json:"nome" binding:"required"`
 	Telefone       string    `json:"telefone" binding:"required"`
-	Responsavel    bool      `json:"responsavel" binding:"required"`
-	Proprietario   bool      `json:"proprietario" binding:"required"`
+	Responsavel    bool      `json:"responsavel"`
+	Proprietario   bool      `json:"proprietario"`
 }
 
 type Morador struct {
