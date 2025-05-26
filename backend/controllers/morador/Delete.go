7827116e436c92755a71639785bffccb023874ec
Thiago Tolotti <thiagotolotti@gmail.com>
2025-05-26 00:25:35 -0300

    applies errs package in delete morador request

--- backend/controllers/morador/Delete.go
+++ backend/controllers/morador/Delete.go
@@ -1,9 +1,9 @@
 package moradorController
 
 import (
+	"backend/errs"
 	moradorModel "backend/models/morador"
 	"backend/utils/cpf"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,22 +12,15 @@ import (
 func Delete(c *gin.Context) {
 	try := c.Param("cpf")
 	cpf, err := cpf.New(try)
-
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "CPF Inválido"})
+		errs.HandleError(c, errs.BadRequest("CPF inválido", err))
 		return
 	}
 
-	success, err := moradorModel.Delete(cpf)
+	appErr := moradorModel.Delete(cpf)
 
-	if err != nil {
-		fmt.Println("Error deleting morador: ", err)
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "Internal Server Error"})
-		return
-	}
-
-	if !success {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "CPF Inválido"})
+	if appErr != nil {
+		errs.HandleError(c, appErr)
 		return
 	}
 
